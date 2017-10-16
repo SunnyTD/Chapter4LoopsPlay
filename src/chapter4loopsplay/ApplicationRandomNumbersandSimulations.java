c58c1fd1740462440d23345a86f1a3abfbae827c
SunnyTD <djplotzke@gmail.com>
2017-10-16 13:24:03 -0700

    Added 4.10: Application: Random Numbers and Simulations

--- src/chapter4loopsplay/ApplicationRandomNumbersandSimulations.java
+++ src/chapter4loopsplay/ApplicationRandomNumbersandSimulations.java
@@ -10,7 +10,12 @@ public class ApplicationRandomNumbersandSimulations
     {
         //<editor-fold defaultstate = "collapsed" desc = "4.10: Application: Random Numbers and Simulations"
 
-
+        // Calls Math.random() 10 times. This generates 10 random floating-point numbers that are >= 0 and <1.
+        for (int i = 1; i <= 10; i++)
+        {
+            double r = Math.random();
+            System.out.println(r);
+        }
         //</editor-fold
     }
 }
